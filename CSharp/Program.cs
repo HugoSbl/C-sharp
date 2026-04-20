10689602af3980c9a88e4bc23367798ce250a13f
Hugo Sablot <hugo.sablot@gmail.com>
2026-04-20 13:26:30 +0200

    first push

---
+++ CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+Console.WriteLine("Calcul de performance.");
+
+Parallel.For(0, 100, b =>
+{
+    Console.WriteLine($"tache {b}");
+    var sw = Stopwatch.StartNew();
+// on éxécute 50 millions de calculs
+    double sum = 1;
+    for (int i = 0; i < 500_000_00; i++)
+    {
+//cosinus
+        sum += Math.Sin(i) + Math.Cos(i);
+//Racine carrée
+        sum += Math.Sqrt(i);
+// Exp + Log
+        sum += Math.Exp(i % 10) + Math.Log(i+1);
+//Puissances
+        sum += Math.Pow(i % 100, 3);
+//Multiplication rule
+        sum *= 1.0000001;
+    }
+
+    sw.Stop();
+    Console.WriteLine($"Temps de calcul {b}: {sw.ElapsedMilliseconds} ms");
+});
+
---
+++ CSharp/README.md
@@ -0,0 +1,3 @@
+Suite à réinitialisation de mon mac, j'ai perdu les anciens projets qui étaient dans le dossier rider plutot que dans mon dossier de dev, qui lui était save. 
+
+### 
